7e8779ba280e48b6eb027fea81c2eaffa7982609
Khai Ray <ckrgrammm@gmail.com>
2024-03-13 00:05:43 +0800

    Feat : Index and pitching first design

--- src/pages/pitching/Pitching.jsx
+++ src/pages/pitching/Pitching.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styles from './pitching.module.css';
 
 function Pitching() {
     return (
-        <div>
-            <h1>Welcome to the Pitching Page</h1>
-            <p>This is a simple page for pitching.</p>
+        <div className={styles.wrapper}>
+        <div className={styles.items}>
+            <div className={styles.item} tabIndex="0" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+            <div className={styles.item} tabIndex="1" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+            <div className={styles.item} tabIndex="2" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+            <div className={styles.item} tabIndex="3" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+            <div className={styles.item} tabIndex="4" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+            <div className={styles.item} tabIndex="5" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+            <div className={styles.item} tabIndex="6" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+            <div className={styles.item} tabIndex="7" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+            <div className={styles.item} tabIndex="8" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+            <div className={styles.item} tabIndex="9" style={{ backgroundImage: 'url(https://res.cloudinary.com/highflyer910/image/upload/v1603650794/vrpics/6.png)' }}></div>
+          </div>
         </div>
     );
 }
